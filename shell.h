981996a8e9637796007a81e0f4fd796af19d1aa5
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-08-19 08:47:14 +0200

    Done

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+void print_prompt();
+#endif
